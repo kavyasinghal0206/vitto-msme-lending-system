2c87d97c6e15f5a0e1f11c23c1f44ca9602dd718
kavyasinghal0206 <singhalkavya0206@gmail.com>
2026-05-09 21:04:48 +0530

    fixed render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
